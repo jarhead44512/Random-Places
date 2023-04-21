8e6d3bc37dd5cb0da57163f367cbca966e9c3762
jarhead44512 <62478215+jarhead44512@users.noreply.github.com>
2023-04-21 15:01:44 -0700

    Second stage

--- js/app.js
+++ js/app.js
@@ -1,38 +1,22 @@
 
-//when btn is presser display = none for first section and second section remove display.
+// 
 
-
-let btnJStyle = document.createElement("button");
-btnJStyle.innerHTML = "Order Up";
-btnJStyle.id = "jsBtn";
-btnJStyle.style.margin = "0 auto"
-document.body.appendChild(btnJStyle);
-
-let reRoll = document.createElement('button');
-reRoll.innerHTML = 'Retry';
-reRoll.id = 'reRoll';
-reRoll.style.margin = "0 auto"
-reRoll.classList.add("disabled");
-document.body.appendChild(reRoll);
-
-let bBtn = document.createElement('button');
-bBtn.innerHTML = 'New Search';
-bBtn.id = 'bBtn';
-bBtn.style.margin = "0 auto"
-bBtn.classList.add("disabled");
-document.body.appendChild(bBtn);
-
+//function for first page button to hide first page content and make second page content appear.
+btnJStyle.onclick = () => {
 let scrn1 = document.getElementById("firstPage");
 let scrn2 = document.getElementById("secondPage");
-
-
-btnJStyle.onclick = () => {
     btnJStyle.style.display = "none";
     scrn1.classList.toggle("disabled");
     scrn2.classList.remove("disabled");
     reRoll.classList.toggle("enabled");
-    reRoll.classList.add('flex');
     bBtn.classList.toggle("enabled");
-    bBtn.classList.add('flex');
 }
 
+
+bBtn.onclick = () => {
+    document.location.reload();
+}
+
+reRoll.onclick = () => {
+    //add results
+}
